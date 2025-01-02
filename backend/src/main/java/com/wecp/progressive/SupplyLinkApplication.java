80eb2c50048748df142c4320d395fcfff4a46602
username <wecp-dummy@gmail.com>
2025-01-02 13:08:49 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/SupplyLinkApplication.java
+++ backend/src/main/java/com/wecp/progressive/SupplyLinkApplication.java
@@ -1,5 +1,8 @@
 package com.wecp.progressive;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SupplyLinkApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to SupplyLink Project!");
